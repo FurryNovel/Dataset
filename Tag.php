87dcbdcaf64bb30fbee1eb78609a9babb0bd5488
k99k5 <k99k5@qq.com>
2024-02-23 10:48:15 +0800

    first commit

---
+++ Tag.php
@@ -0,0 +1,1240 @@
+<?php
+return [
+	'novel' => [
+		'zh_CN' => [
+			'小说',
+		],
+		'zh_TW' => [
+			'小説',
+		],
+		'en_US' => [
+			'novel',
+		],
+	],
+	'txt' => [
+		'zh_CN' => [
+			'txt',
+		],
+		'zh_TW' => [
+			'txt',
+		],
+		'en_US' => [
+			'txt',
+		],
+	],
+	'docx' => [
+		'zh_CN' => [
+			'docx',
+		],
+		'zh_TW' => [
+			'docx',
+		],
+		'en_US' => [
+			'docx',
+		],
+	],
+	'translated' => [
+		'zh_CN' => [
+			'翻译',
+		],
+		'zh_TW' => [
+			'翻译',
+		],
+		'en_US' => [
+			'translated',
+		],
+	],
+	'en' => [
+		'zh_CN' => [
+			'en',
+			'英语',
+		],
+		'zh_TW' => [
+			'en',
+			'英語',
+		],
+		'en_US' => [
+			'en',
+		],
+	],
+	'zh' => [
+		'zh_CN' => [
+			'中文',
+			'中国语',
+		],
+		'zh_TW' => [
+			'中文',
+			'中国語',
+		],
+		'en_US' => [
+			'zh',
+		],
+	],
+	'zh_cn' => [
+		'zh_CN' => [
+			'简体中文',
+		],
+		'zh_TW' => [
+			'簡體中文',
+		],
+		'en_US' => [
+			'zh_cn',
+		],
+	],
+	'zh_tw' => [
+		'zh_CN' => [
+			'繁體中文',
+		],
+		'zh_TW' => [
+			'繁體中文',
+		],
+		'en_US' => [
+			'zh_tw',
+		],
+	],
+	'ja' => [
+		'zh_CN' => [
+			'日语',
+		],
+		'zh_TW' => [
+			'日語',
+		],
+		'en_US' => [
+			'ja',
+		],
+	],
+	'ko' => [
+		'zh_CN' => [
+			'韩语',
+		],
+		'zh_TW' => [
+			'韓語',
+		],
+		'en_US' => [
+			'ko',
+		],
+	],
+	'Original' => [
+		'zh_CN' => [
+			'原创',
+		],
+		'zh_TW' => [
+			'原創',
+		],
+		'en_US' => [
+			'Original',
+		],
+	],
+	'Doujin' => [
+		'zh_CN' => [
+			'同人',
+		],
+		'zh_TW' => [
+			'同人',
+		],
+		'en_US' => [
+			'Doujin',
+		],
+	],
+	'SFW' => [
+		'zh_CN' => [
+			'SFW',
+			'适合在工作场所观看',
+		],
+		'zh_TW' => [
+			'SFW',
+			'適合在工作場所觀看',
+		],
+		'en_US' => [
+			'SFW',
+		],
+	],
+	'NSFW' => [
+		'zh_CN' => [
+			'NSFW',
+			'不适合在工作场所观看',
+		],
+		'zh_TW' => [
+			'NSFW',
+			'不適合在工作場所觀看',
+		],
+		'en_US' => [
+			'NSFW',
+		],
+	],
+	'R18' => [
+		'zh_CN' => [
+			'R18',
+			'限制级',
+		],
+		'zh_TW' => [
+			'R18',
+			'限制級',
+		],
+		'en_US' => [
+			'R18',
+		],
+	],
+	'R18G' => [
+		'zh_CN' => [
+			'R18G',
+			'限制级',
+		],
+		'zh_TW' => [
+			'R18G',
+			'限制級',
+		],
+		'en_US' => [
+			'R18G',
+		],
+	],
+	'Gay' => [
+		'zh_CN' => [
+			'同性恋',
+		],
+		'zh_TW' => [
+			'同性戀',
+		],
+		'en_US' => [
+			'Gay',
+		],
+	],
+	'Lesbian' => [
+		'zh_CN' => [
+			'Lesbian',
+			'女同性恋',
+		],
+		'zh_TW' => [
+			'Lesbian',
+			'女同性戀',
+		],
+		'en_US' => [
+			'Lesbian',
+		],
+	],
+	'General' => [
+		'zh_CN' => [
+			'General',
+			'一般',
+		],
+		'zh_TW' => [
+			'General',
+			'一般',
+		],
+		'en_US' => [
+			'General',
+		],
+	],
+	'Furry' => [
+		'zh_CN' => [
+			'兽人',
+		],
+		'zh_TW' => [
+			'獸人',
+			'獣人',
+		],
+		'en_US' => [
+			'Furry',
+		],
+		'ja_JP' => [
+			'kemono',
+			'じゅうじん',
+			'けもの',
+			'ケモノ',
+			'オスケモ',
+			'メスケモ',
+		],
+	],
+	'Gay furry' => [
+		'zh_CN' => [
+			'兽人同性',
+		],
+		'zh_TW' => [
+			'獸人同性',
+		],
+		'en_US' => [
+			'Gay furry',
+		],
+		'ja_JP' => [
+			'ケモホモ',
+			'ホモケモ',
+		],
+	],
+	'Chibi Furry' => [
+		'zh_CN' => [
+			'兽人正太',
+			'兽太',
+		],
+		'zh_TW' => [
+			'獸人正太',
+			'獸太',
+		],
+		'en_US' => [
+			'Chibi Furry',
+		],
+		'ja_JP' => [
+			'ケモロリ',
+			'ケモショタ',
+			'チビケモ',
+		],
+	],
+	'Romance' => [
+		'zh_CN' => [
+			'恋爱',
+		],
+		'zh_TW' => [
+			'戀愛',
+		],
+		'en_US' => [
+			'Romance',
+		],
+	],
+	'Human' => [
+		'zh_CN' => [
+			'人类',
+		],
+		'zh_TW' => [
+			'人類',
+		],
+		'en_US' => [
+			'Human',
+		],
+	],
+	'Robot' => [
+		'zh_CN' => [
+			'机器人',
+		],
+		'zh_TW' => [
+			'機器人',
+		],
+		'en_US' => [
+			'Robot',
+		],
+	],
+	'Slime' => [
+		'zh_CN' => [
+			'史莱姆',
+		],
+		'zh_TW' => [
+			'史萊姆',
+		],
+		'en_US' => [
+			'Slime',
+		],
+	],
+	'Tentacles' => [
+		'zh_CN' => [
+			'触手',
+		],
+		'zh_TW' => [
+			'觸手',
+		],
+		'en_US' => [
+			'Tentacles',
+		],
+	],
+	'Vampire' => [
+		'zh_CN' => [
+			'吸血鬼',
+		],
+		'zh_TW' => [
+			'吸血鬼',
+		],
+		'en_US' => [
+			'Vampire',
+		],
+	],
+	'Hero' => [
+		'zh_CN' => [
+			'英雄',
+		],
+		'zh_TW' => [
+			'英雄',
+		],
+		'en_US' => [
+			'Hero',
+		],
+	],
+	'Police' => [
+		'zh_CN' => [
+			'警察',
+			'特警',
+		],
+		'zh_TW' => [
+			'警察',
+			'特警',
+		],
+		'en_US' => [
+			'Police',
+		],
+	],
+	'DevilKing' => [
+		'zh_CN' => [
+			'魔王',
+		],
+		'zh_TW' => [
+			'魔王',
+		],
+		'en_US' => [
+			'DevilKing',
+		],
+	],
+	'Brave' => [
+		'zh_CN' => [
+			'勇者',
+		],
+		'zh_TW' => [
+			'勇者',
+		],
+		'en_US' => [
+			'Brave',
+		],
+	],
+	'Succubus' => [
+		'zh_CN' => [
+			'魅魔',
+		],
+		'zh_TW' => [
+			'魅魔',
+		],
+		'en_US' => [
+			'Succubus',
+		],
+	],
+	'VaginalSex' => [
+		'zh_CN' => [
+			'阴交',
+			'阴道',
+		],
+		'zh_TW' => [
+			'陰交',
+			'陰道',
+		],
+		'en_US' => [
+			'VaginalSex',
+		],
+	],
+	'Anal' => [
+		'zh_CN' => [
+			'肛交',
+			'后入',
+		],
+		'zh_TW' => [
+			'肛交',
+			'后入',
+		],
+		'en_US' => [
+			'Anal',
+		],
+	],
+	'69BlowJob' => [
+		'zh_CN' => [
+			'六九',
+			'互口',
+		],
+		'zh_TW' => [
+			'六九',
+			'互口',
+		],
+		'en_US' => [
+			'69BlowJob',
+			'69',
+			'69 BlowJob',
+		],
+	],
+	'BlowJob' => [
+		'zh_CN' => [
+			'口交',
+		],
+		'zh_TW' => [
+			'口交',
+		],
+		'en_US' => [
+			'BlowJob',
+		],
+	],
+	'Cloaca' => [
+		'zh_CN' => [
+			'生殖缝',
+			'生殖腔',
+			'生殖裂',
+		],
+		'zh_TW' => [
+			'生殖缝',
+			'生殖腔',
+			'生殖裂',
+		],
+		'en_US' => [
+			'Cloaca',
+		],
+	],
+	'TailJob' => [
+		'zh_CN' => [
+			'尾交',
+		],
+		'zh_TW' => [
+			'尾交',
+		],
+		'en_US' => [
+			'TailJob',
+		],
+	],
+	'Masturbation' => [
+		'zh_CN' => [
+			'自慰',
+		],
+		'zh_TW' => [
+			'自慰',
+		],
+		'en_US' => [
+			'Masturbation',
+		],
+	],
+	'Fisting' => [
+		'zh_CN' => [
+			'拳交',
+		],
+		'zh_TW' => [
+			'拳交',
+		],
+		'en_US' => [
+			'Fisting',
+		],
+	],
+	'FootJob' => [
+		'zh_CN' => [
+			'足交',
+		],
+		'zh_TW' => [
+			'足交',
+		],
+		'en_US' => [
+			'FootJob',
+		],
+	],
+	'Paw' => [
+		'zh_CN' => [
+			'爪子',
+			'手爪',
+			'脚爪',
+		],
+		'zh_TW' => [
+			'爪子',
+			'手爪',
+			'脚爪',
+		],
+		'en_US' => [
+			'Paw',
+		],
+	],
+	'SockJob' => [
+		'zh_CN' => [
+			'袜交',
+			'白袜',
+		],
+		'zh_TW' => [
+			'袜交',
+			'白袜',
+		],
+		'en_US' => [
+			'SockJob',
+		],
+	],
+	'UrethraInsertion' => [
+		'zh_CN' => [
+			'尿道插入',
+		],
+		'zh_TW' => [
+			'尿道插入',
+		],
+		'en_US' => [
+			'UrethraInsertion',
+		],
+	],
+	'tickling' => [
+		'zh_CN' => [
+			'搔痒',
+			'挠痒',
+		],
+		'zh_TW' => [
+			'搔痒',
+			'挠痒',
+		],
+		'en_US' => [
+			'tickling',
+		],
+	],
+	'Rape' => [
+		'zh_CN' => [
+			'强奸',
+			'强上',
+		],
+		'zh_TW' => [
+			'强奸',
+			'强上',
+		],
+		'en_US' => [
+			'Rape',
+		],
+	],
+	'Incest' => [
+		'zh_CN' => [
+			'乱伦',
+		],
+		'zh_TW' => [
+			'乱伦',
+		],
+		'en_US' => [
+			'Incest',
+		],
+	],
+	'Group' => [
+		'zh_CN' => [
+			'群交',
+			'群p',
+			'轮奸',
+		],
+		'zh_TW' => [
+			'群交',
+			'群p',
+			'轮奸',
+		],
+		'en_US' => [
+			'Group',
+			'3p',
+			'np',
+		],
+	],
+	'Cuckold' => [
+		'zh_CN' => [
+			'绿帽',
+		],
+		'zh_TW' => [
+			'绿帽',
+		],
+		'en_US' => [
+			'Cuckold',
+			'netorare',
+			'ntr',
+		],
+	],
+	'Oyakodon' => [
+		'zh_CN' => [
+			'父子丼',
+		],
+		'zh_TW' => [
+			'父子丼',
+		],
+		'en_US' => [
+			'Oyakodon',
+		],
+	],
+	'brother' => [
+		'zh_CN' => [
+			'兄弟丼',
+		],
+		'zh_TW' => [
+			'兄弟丼',
+		],
+		'en_US' => [
+			'brother',
+		],
+	],
+	'BDSM' => [
+		'zh_CN' => [
+			'调教',
+		],
+		'zh_TW' => [
+			'调教',
+		],
+		'en_US' => [
+			'BDSM',
+			'SM',
+		],
+	],
+	'Bondage' => [
+		'zh_CN' => [
+			'捆绑',
+			'束缚',
+			'绑缚',
+		],
+		'zh_TW' => [
+			'捆绑',
+			'束缚',
+			'绑缚',
+		],
+		'en_US' => [
+			'Bondage',
+		],
+	],
+	'ElectricShocks' => [
+		'zh_CN' => [
+			'电击',
+		],
+		'zh_TW' => [
+			'电击',
+		],
+		'en_US' => [
+			'ElectricShocks',
+		],
+	],
+	'MindControl' => [
+		'zh_CN' => [
+			'精神控制',
+			'精控',
+			'洗脑',
+		],
+		'zh_TW' => [
+			'精神控制',
+			'精控',
+			'洗脑',
+		],
+		'en_US' => [
+			'MindControl',
+			'BrainWash',
+			'BrainWashing',
+		],
+	],
+	'Asphyxiation' => [
+		'zh_CN' => [
+			'窒息',
+			'呼吸控制',
+			'控制呼吸',
+		],
+		'zh_TW' => [
+			'窒息',
+			'呼吸控制',
+			'控制呼吸',
+		],
+		'en_US' => [
+			'Asphyxiation',
+		],
+	],
+	'OrgasmDenial' => [
+		'zh_CN' => [
+			'控制射精',
+			'射精控制',
+		],
+		'zh_TW' => [
+			'控制射精',
+			'射精控制',
+		],
+		'en_US' => [
+			'OrgasmDenial',
+		],
+	],
+	'Slave' => [
+		'zh_CN' => [
+			'主奴',
+			'性奴',
+			'奴隶',
+		],
+		'zh_TW' => [
+			'主奴',
+			'性奴',
+			'奴隶',
+		],
+		'en_US' => [
+			'Slave',
+		],
+	],
+	'CrotchTattoo' => [
+		'zh_CN' => [
+			'淫纹',
+		],
+		'zh_TW' => [
+			'淫纹',
+		],
+		'en_US' => [
+			'CrotchTattoo',
+		],
+	],
+	'PublicUse' => [
+		'zh_CN' => [
+			'RBQ',
+			'肉便器',
+		],
+		'zh_TW' => [
+			'RBQ',
+			'肉便器',
+		],
+		'en_US' => [
+			'PublicUse',
+			'Public',
+		],
+	],
+	'PissDrinking' => [
+		'zh_CN' => [
+			'圣水',
+		],
+		'zh_TW' => [
+			'圣水',
+		],
+		'en_US' => [
+			'PissDrinking',
+		],
+	],
+	'Coprophagia' => [
+		'zh_CN' => [
+			'黄金',
+		],
+		'zh_TW' => [
+			'黄金',
+		],
+		'en_US' => [
+			'Coprophagia',
+		],
+	],
+	'Forniphilia' => [
+		'zh_CN' => [
+			'物化',
+		],
+		'zh_TW' => [
+			'物化',
+		],
+		'en_US' => [
+			'Forniphilia',
+		],
+	],
+	'WeightGain' => [
+		'zh_CN' => [
+			'育肥',
+			'肥满化',
+			'肥満化',
+		],
+		'zh_TW' => [
+			'育肥',
+			'肥满化',
+			'肥満化',
+		],
+		'en_US' => [
+			'WeightGain',
+		],
+	],
+	'BattleDamage' => [
+		'zh_CN' => [
+			'战损',
+			'战败',
+			'欠損',
+		],
+		'zh_TW' => [
+			'战损',
+			'战败',
+			'欠損',
+		],
+		'en_US' => [
+			'BattleDamage',
+		],
+	],
+	'BodyModification' => [
+		'zh_CN' => [
+			'身体改造',
+		],
+		'zh_TW' => [
+			'身体改造',
+		],
+		'en_US' => [
+			'BodyModification',
+		],
+	],
+	'Transfur Furry' => [
+		'zh_CN' => [
+			'兽化',
+			'同化',
+		],
+		'zh_TW' => [
+			'兽化',
+			'同化',
+		],
+		'en_US' => [
+			'Transfur Furry',
+			'Transfur',
+			'TF',
+		],
+	],
+	'Transfur Dragon' => [
+		'zh_CN' => [
+			'龙化',
+		],
+		'zh_TW' => [
+			'龙化',
+		],
+		'en_US' => [
+			'Transfur Dragon',
+		],
+	],
+	'Petrification' => [
+		'zh_CN' => [
+			'石化',
+		],
+		'zh_TW' => [
+			'石化',
+		],
+		'en_US' => [
+			'Petrification',
+		],
+	],
+	'corruption' => [
+		'zh_CN' => [
+			'堕落',
+			'恶堕',
+		],
+		'zh_TW' => [
+			'堕落',
+			'恶堕',
+		],
+		'en_US' => [
+			'corruption',
+		],
+	],
+	'corruption hero' => [
+		'zh_CN' => [
+			'英雄恶堕',
+		],
+		'zh_TW' => [
+			'英雄恶堕',
+		],
+		'en_US' => [
+			'corruption hero',
+		],
+	],
+	'Hypnosis' => [
+		'zh_CN' => [
+			'催眠',
+		],
+		'zh_TW' => [
+			'催眠',
+		],
+		'en_US' => [
+			'Hypnosis',
+		],
+	],
+	'Smell' => [
+		'zh_CN' => [
+			'Smell',
+		],
+		'zh_TW' => [
+			'Smell',
+		],
+		'en_US' => [
+			'Smell',
+		],
+	],
+	'Drugs' => [
+		'zh_CN' => [
+			'Drugs',
+		],
+		'zh_TW' => [
+			'Drugs',
+		],
+		'en_US' => [
+			'Drugs',
+		],
+	],
+	'BodySwap' => [
+		'zh_CN' => [
+			'灵魂交换',
+			'身体交换',
+		],
+		'zh_TW' => [
+			'灵魂交换',
+			'身体交换',
+		],
+		'en_US' => [
+			'BodySwap',
+		],
+	],
+	'Machine' => [
+		'zh_CN' => [
+			'机械奸',
+		],
+		'zh_TW' => [
+			'机械奸',
+		],
+		'en_US' => [
+			'Machine',
+		],
+	],
+	'Drone' => [
+		'zh_CN' => [
+			'Drone',
+		],
+		'zh_TW' => [
+			'Drone',
+		],
+		'en_US' => [
+			'Drone',
+		],
+	],
+	'Diaper' => [
+		'zh_CN' => [
+			'尿布',
+		],
+		'zh_TW' => [
+			'尿布',
+		],
+		'en_US' => [
+			'Diaper',
+		],
+	],
+	'Latex' => [
+		'zh_CN' => [
+			'乳胶衣',
+			'胶衣',
+		],
+		'zh_TW' => [
+			'乳胶衣',
+			'胶衣',
+		],
+		'en_US' => [
+			'Latex',
+		],
+	],
+	'Rubber' => [
+		'zh_CN' => [
+			'胶液',
+			'胶',
+		],
+		'zh_TW' => [
+			'胶液',
+			'胶',
+		],
+		'en_US' => [
+			'Rubber',
+		],
+	],
+	'Armor' => [
+		'zh_CN' => [
+			'盔甲',
+			'铠甲',
+		],
+		'zh_TW' => [
+			'盔甲',
+			'铠甲',
+		],
+		'en_US' => [
+			'Armor',
+		],
+	],
+	'PowerArmor' => [
+		'zh_CN' => [
+			'动力装甲',
+		],
+		'zh_TW' => [
+			'动力装甲',
+		],
+		'en_US' => [
+			'PowerArmor',
+		],
+	],
+	'Socks' => [
+		'zh_CN' => [
+			'袜子',
+			'白袜',
+		],
+		'zh_TW' => [
+			'袜子',
+			'白袜',
+		],
+		'en_US' => [
+			'Socks',
+		],
+	],
+	'Muscles' => [
+		'zh_CN' => [
+			'肌肉',
+			'筋肉',
+		],
+		'zh_TW' => [
+			'肌肉',
+			'筋肉',
+		],
+		'en_US' => [
+			'Muscles',
+		],
+	],
+	'Hyper' => [
+		'zh_CN' => [
+			'巨根',
+		],
+		'zh_TW' => [
+			'巨根',
+		],
+		'en_US' => [
+			'Hyper',
+		],
+	],
+	'Macro' => [
+		'zh_CN' => [
+			'巨大化',
+		],
+		'zh_TW' => [
+			'巨大化',
+		],
+		'en_US' => [
+			'Macro',
+		],
+	],
+	'SizeDifferent' => [
+		'zh_CN' => [
+			'体型差',
+			'体格差',
+		],
+		'zh_TW' => [
+			'体型差',
+			'体格差',
+		],
+		'en_US' => [
+			'SizeDifferent',
+		],
+	],
+	'Vore' => [
+		'zh_CN' => [
+			'吞食',
+			'丸吞',
+		],
+		'zh_TW' => [
+			'吞食',
+			'丸吞',
+		],
+		'en_US' => [
+			'Vore',
+		],
+	],
+	'CockVore' => [
+		'zh_CN' => [
+			'CockVore',
+		],
+		'zh_TW' => [
+			'CockVore',
+		],
+		'en_US' => [
+			'CockVore',
+		],
+	],
+	'AnalVore' => [
+		'zh_CN' => [
+			'AnalVore',
+		],
+		'zh_TW' => [
+			'AnalVore',
+		],
+		'en_US' => [
+			'AnalVore',
+		],
+	],
+	'TailVore' => [
+		'zh_CN' => [
+			'TailVore',
+		],
+		'zh_TW' => [
+			'TailVore',
+		],
+		'en_US' => [
+			'TailVore',
+		],
+	],
+	'Eggs' => [
+		'zh_CN' => [
+			'产卵',
+			'下蛋',
+		],
+		'zh_TW' => [
+			'产卵',
+			'下蛋',
+		],
+		'en_US' => [
+			'Eggs',
+		],
+	],
+	'MaleBirth' => [
+		'zh_CN' => [
+			'雄妊娠',
+			'雄性妊娠',
+		],
+		'zh_TW' => [
+			'雄妊娠',
+			'雄性妊娠',
+		],
+		'en_US' => [
+			'MaleBirth',
+		],
+	],
+	'Inflation' => [
+		'zh_CN' => [
+			'膨胀',
+		],
+		'zh_TW' => [
+			'膨胀',
+		],
+		'en_US' => [
+			'Inflation',
+		],
+	],
+	'GutTorture' => [
+		'zh_CN' => [
+			'虐腹',
+		],
+		'zh_TW' => [
+			'虐腹',
+		],
+		'en_US' => [
+			'GutTorture',
+		],
+	],
+	'R18G CBT' => [
+		'zh_CN' => [
+			'虐屌',
+		],
+		'zh_TW' => [
+			'虐屌',
+		],
+		'en_US' => [
+			'R18G CBT',
+		],
+	],
+	'R18G Blood' => [
+		'zh_CN' => [
+			'流血',
+		],
+		'zh_TW' => [
+			'流血',
+		],
+		'en_US' => [
+			'R18G Blood',
+		],
+	],
+	'R18G Mutilation' => [
+		'zh_CN' => [
+			'断肢',
+		],
+		'zh_TW' => [
+			'斷肢',
+		],
+		'en_US' => [
+			'R18G Mutilation',
+		],
+	],
+	'R18G Castrated' => [
+		'zh_CN' => [
+			'阉割',
+		],
+		'zh_TW' => [
+			'阉割',
+		],
+		'en_US' => [
+			'R18G Castrated',
+		],
+	],
+	'AI Generated' => [
+		'zh_CN' => [
+			'AI生成',
+		],
+		'zh_TW' => [
+			'AI生成',
+		],
+		'en_US' => [
+			'AI Generated',
+		],
+	],
+];
